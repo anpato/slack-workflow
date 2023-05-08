ac51942f091fb551c953a0147ab912fd6c77fa8b
Andre Pato <anpato1994@gmail.com>
2023-05-08 17:04:24 -0400

    add more tests

--- index.js
+++ index.js
@@ -2,11 +2,30 @@ module.exports = {
     logName(){
         return 'Name'
     },
-    countTo10(){
-        let arr = []
-        for (let i = 1; i <=10 ; i++) {
-            arr.push(i)
+    fib(n){
+        let n1 = 0, n2 = 1, nextTerm;
+
+        console.log('Fibonacci Series:');
+
+        for (let i = 1; i <= n; i++) {
+            console.log(n1);
+            nextTerm = n1 + n2;
+            n1 = n2;
+            n2 = nextTerm;
         }
+        return Math.floor(n1)
+    },
+    async countTo10(){
+        let arr = []
+
+        for (let i = 1; i <=10 ; i++) {
+           await  new Promise(resolve => setTimeout(()=> {
+                arr.push(i)
+                resolve()
+            }), 2000)
+
+        }
+
         return arr
     }
 }
--- index.test.js
+++ index.test.js
@@ -1,11 +1,20 @@
 const methods = require('./index')
 describe('My Test', function () {
-    it('should count to 10', ()=> {
-        const nums = methods.countTo10()
+    beforeEach(()=> {
+        jest.setTimeout(4000)
+    })
+
+    it('should count to 10',async ()=> {
+        const nums = await methods.countTo10()
         expect(nums).toEqual([1,2,3,4,5,6,7,8,9,10])
     })
 
     it('should return string of name', () =>{
         expect(methods.logName()).toBe("Name")
+    })
+
+    it('should do fib', ()=> {
+        const fib = methods.fib(100)
+        expect(fib).toBe(354224848179262000000)
     })
 });
